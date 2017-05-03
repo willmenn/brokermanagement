460f1bcb30fe41ca5ea4a2698942d46e25016fb1
William Ahrons <william.ahrons@gmail.com>
2017-05-03 20:37:42 -0300

    Create baseic files from project

---
+++ src/main/java/com/brokermanagement/BrokermanagementApplication.java
@@ -0,0 +1,12 @@
+package com.brokermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokermanagementApplication.class, args);
+	}
+}
