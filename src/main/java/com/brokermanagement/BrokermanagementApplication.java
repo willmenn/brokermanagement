5fdcd1c9580e5d232c5e86c137551adf8ae265e0
William Ahrons <william.ahrons@gmail.com>
2017-06-04 00:44:26 -0300

    Enable CORS

--- src/main/java/com/brokermanagement/BrokermanagementApplication.java
+++ src/main/java/com/brokermanagement/BrokermanagementApplication.java
@@ -2,11 +2,19 @@ package com.brokermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class BrokermanagementApplication {
+public class BrokermanagementApplication extends WebMvcConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BrokermanagementApplication.class, args);
-	}
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+	
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrokermanagementApplication.class, args);
+    }
 }
