626f3d4b9f74f7cfb5d3ebc9b9a9379ea61c1cb6
William Ahrons <william.ahrons@gmail.com>
2017-09-10 00:20:07 -0300

    Add ID into Manager model to map correctly within the db.

--- src/main/java/com/brokermanagement/model/Manager.java
+++ src/main/java/com/brokermanagement/model/Manager.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
@@ -13,6 +14,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Builder
 @Document(collection = "manager")
 public class Manager {
+    @Id
+    private String id;
     private String manager;
     private String password;
     @Setter
